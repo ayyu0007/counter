c734860f4b0fcd7ca9f20d5622d97d669f6ada09
ayush <ayushrangani71530@gmail.com>
2023-10-23 14:36:51 +0530

    counter_react

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
+
+
 
 function App() {
+  const [count, setcount] = useState(0);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   <div className='container my-6' >
+    <div className='card text-center my-6'>
+      <div className='card-body card bg-dark text-white mb-3' >
+        <h1>COUNTER </h1><br/>
+        <div className='my-6'>
+          <h2 className='my-6'>{count}</h2>
+          <button className='btn btn-success mx-3' onClick={() => setcount(count + 1)}>INCREMENT</button>
+          <button className='btn btn-danger mx-3' onClick={() => setcount(count - 1)} >DECREMENT</button>
+        </div> 
+      </div>
+
     </div>
+    
+   </div>
   );
 }
 
